67652ce8d9d4ba76ad5a7ef5adb55e8807c4c2ce
Alex <stefan.alex170299@gmail.com>
2024-08-22 19:34:32 +0300

    update network

--- Linux/data-migration.sh
+++ Linux/data-migration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name solr-curl-container --network host --rm alexstefan1702/solr-curl-update
+docker run --name solr-curl-container --network mynetwork --ip 172.18.0.14 --rm alexstefan1702/solr-curl-update
