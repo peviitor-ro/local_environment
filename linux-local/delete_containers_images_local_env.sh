25c68b715586da40aea4c7a7048c3ea9e7ebc9f4
Ifrim Alexandra <157494822+ale23yfm@users.noreply.github.com>
2025-08-01 21:44:43 +0300

    Delete linux-local/delete_containers_images_local_env.sh

--- linux-local/delete_containers_images_local_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker stop apache-container solr-container
-docker rm apache-container solr-container
-docker rmi fe:latest sebiboga/peviitor:latest sebiboga/php-apache:latest
-docker rmi solr
-docker rmi alexstefan1702/solr-curl-update 
