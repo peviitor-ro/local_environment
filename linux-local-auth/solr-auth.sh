3c8a37ad21f157b3389caba9cd3c25f97f436516
Cosmin <csgitman@gmail.com>
2025-08-23 00:31:37 +0300

    Update solr-auth.sh

--- linux-local-auth/solr-auth.sh
+++ linux-local-auth/solr-auth.sh
@@ -509,7 +509,9 @@ curl --user $new_user:$new_pass http://localhost:8983/solr/admin/authentication 
 -H 'Content-type:application/json' \
 -d "{\"delete-user\": [\"$old_user\"]}"
 
-cat > informatii_importante.peviitor.txt <<EOF
+FILE="$HOME/Documents/informatii_importante.peviitor.txt"
+mkdir -p "$(dirname "$FILE")"
+cat > "$FILE" <<'EOF'
 =================================================================
                     IMPORTANT INFORMATION
 =================================================================
